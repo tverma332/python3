a167ea58083103b728ef9cd6fe56ccbce6405d55
Tushar verma <tverma332@gmail.com>
2022-02-24 11:32:52 +0530

    add typeconversion dir

---
+++ type_coversion/typeconversion.py
@@ -0,0 +1,11 @@
+# Type casting or type conversion
+# NOTE: Any data type can be converted into boolean | Any data type can be converted into string but reverse is not true
+
+x=True
+print(type(x))
+print(type(str(x)))
+
+y=5.90
+print(type(y))
+print(int(y))
+print(type(y))
