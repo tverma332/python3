cdb4f2530ac2117004c81f2c183faf4d5238edd4
Tushar verma <tverma332@gmail.com>
2022-02-24 09:50:24 +0530

    add specialchar file

---
+++ specialcharacters.py
@@ -0,0 +1,19 @@
+# Usage of Special characters
+# Note: Use Special chars. only in quotes
+
+
+# 1) \n = Next line(Everything written after \n will be print on Next line)
+
+print("Hello\nWorld")
+
+# 2) \b = Back Space
+
+print("Hello \b\bworld")
+
+# 3) \t = tab(Add tab space)
+
+print("Hello\tworld")
+
+# 4) \ = Escape symbol(useful in case of defining paths mainly windows path)
+
+print('Hello this is python\'s class') 
