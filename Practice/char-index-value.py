5485a8a0f625afff90603f698750e24853e2af41
Tushar verma <tverma332@gmail.com>
2022-03-06 13:29:41 +0530

    added chars and index value practice problem

---
+++ Practice/char-index-value.py
@@ -0,0 +1,8 @@
+# LEC 50 : Read a string and print their chars and index values
+
+usr_str = input("Enter your string : ")
+
+index = 0
+for each in usr_str:
+	print(f"{each}-->{index}")
+	index = index + 1
