17b5c953d97a554946053b88ba4332daa0d12367
Tushar verma <tverma332@gmail.com>
2022-03-08 22:11:56 +0530

    added java version practice problem

--- Practice/get-java-version.py
+++ Practice/get-java-version.py
@@ -5,7 +5,7 @@ cmd = 'java -version'
 sp = subprocess.Popen(cmd , shell=True , stdout=subprocess.PIPE , stderr=subprocess.PIPE , universal_newlines=True)
 rc = sp.wait()
 o,e = sp.communicate()
-if rc == 0:
+if rc == 0 :
 	if bool(o) == False and bool(e) == True: # even when the command was success you're getting output in error , it is not problem with your python it's compatibility with java and operating system
 		print(e.splitlines()[0].split()[2].strip("\""))
 else:
