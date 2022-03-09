ae80f3e78ed67c6e4e7adb6c162014872a57b134
Tushar verma <tverma332@gmail.com>
2022-03-09 07:01:25 +0530

    modified working with text files

--- working_with_text_files/write-a-file.py
+++ working_with_text_files/write-a-file.py
@@ -3,7 +3,7 @@
 
 fo = open ("random" , "w")
 
-fo.write("This is random text file\n")
+fo.write("This is random text file\n") # \n is similar to press enter to take your cursor to next line so you can write from next line the new text
 fo.write("This is second line")
 
 fo.close()
