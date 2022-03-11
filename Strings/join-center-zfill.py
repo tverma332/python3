9272d06eb847af2b463b9022f29b794c0b8a384e
Tushar verma <tverma332@gmail.com>
2022-03-11 19:18:09 +0530

    update join center file

--- Strings/join-center-zfill.py
+++ Strings/join-center-zfill.py
@@ -6,11 +6,11 @@ print(y)
 
 # 2) center = When you want to print string anywhere in center of given (any_number)
 
-x= "python"
+x = "python"
 y=x.center(34)
 print(y)
 
 # 3) zfill = Pad a numeric string with zeros on the left, to fill a field of the given width
 
-x="python"
+x = "python"
 print(x.zfill(9))
