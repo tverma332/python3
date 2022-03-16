eb73be2c7af980e34ddd35124b5f8c36851069a6
Tushar verma <tverma332@gmail.com>
2022-03-16 19:15:02 +0530

    corrected a typo

--- RegEx/re-finditer.py
+++ RegEx/re-finditer.py
@@ -6,5 +6,5 @@ text = "This is python and we are having python2 and python3 version"
 my_pat = r'\bpython[23]?\b'
 match_ob = re.finditer(my_pat,text)
 
-for each_ob in re.finditer(my_pat,text) :
+for each_ob in re.finditer(my_pat , text) :
 	print(f" The match is : {each_ob.group()} , Starting Index : {each_ob.start()}" )
