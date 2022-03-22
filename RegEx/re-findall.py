e998b35371cba45bfc233820259c48e6cdb10779
Tushar verma <tverma332@gmail.com>
2022-03-22 19:58:13 +0530

    corrected a typo

--- RegEx/re-findall.py
+++ RegEx/re-findall.py
@@ -6,7 +6,6 @@ text = "This is a python and it is easy to learn"
 my_pat = "is"
 
 print(re.findall(my_pat,text))
-
 print(len(re.findall(my_pat,text))) # number of occurence using len function 
 
 # NOTE : We're not searching for words but string (Pattern represents strings)
