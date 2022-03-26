5e58210f6ac6fe26c1e89db03f9e6ecdf808e93d
Tushar verma <tverma332@gmail.com>
2022-03-26 11:29:22 +0530

    added paramiko practice problem

--- Practice/Paramiko-transfer-download-remote-server.py
+++ Practice/Paramiko-transfer-download-remote-server.py
@@ -3,12 +3,12 @@
 import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect( hostname = '138.91.163.228' , username = 'azureuser' , password = 'Carry@minati' , port = 22)
+ssh.connect( hostname = '138.91.163.228' , username = 'azureuser' , password = '*******' , port = 22)
 sftp_client = ssh.open_sftp()
 
-# sftp_client.get('/home/azureuser/lisa' , 'xyx') # Download file form remote server to local server
+# sftp_client.get('/home/azureuser/textfile' , 'test.txt') # Download file form remote server to local server
 
-sftp_client.put('practice.py' , '/home/azureuser/xyz.py') # Transfer file form local server to remote server
+sftp_client.put('file1.py' , '/home/azureuser/xyz.py') # Transfer file form local server to remote server
 
 sftp_client.close()
 ssh.close()
