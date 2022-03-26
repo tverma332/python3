bb6c549fc7b9ce0dde589384db8650e15a2eca7f
Tushar verma <tverma332@gmail.com>
2022-03-26 10:58:59 +0530

    added paramiko module

--- Modules-in-python/Paramiko/working-with-password.py
+++ Modules-in-python/Paramiko/working-with-password.py
@@ -1,7 +1,9 @@
+# Using username and password
+
 import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname = '138.91.163.228' , username = 'azureuser' , password = 'Carry@minati' , port = 22)
+ssh.connect(hostname = '138.91.163.228' , username = 'azureuser' , password = '********' , port = 22)
 stdin , stdout , stderr = ssh.exec_command('whoami')
 
 print("The ouput is: ")
