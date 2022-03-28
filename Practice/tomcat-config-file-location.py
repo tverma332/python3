d4fbbfa8b27f9f58feb62788a4eff8ab96676dc1
Tushar verma <tverma332@gmail.com>
2022-03-28 12:13:43 +0530

    added practice problem 98

--- Practice/tomcat-config-file-location.py
+++ Practice/tomcat-config-file-location.py
@@ -42,3 +42,43 @@ def main():
 if __name__=="__main__":
     main()
 '''
+
+#---------------------------------------- USING OOPS ----------------------------------------------------------------------------------
+
+import os
+
+class Tomcat(object):
+	def get_home_conf_file(self,server_Xml):
+		self.t_home=os.path.dirname(os.path.dirname(server_Xml))
+		self.conf_file=server_Xml
+
+	def display_details(self):
+		print(f"Tomcat home: {self.t_home}")
+		print(f"Tomcat config. file: {self.conf_files}")
+		return None
+
+def get_all_tomcats():
+	list_of_config_files=[]
+	for r,d,f in os.walk("/"):
+		for each_file in f:
+			if each_file=="server.xml":
+				list_of_config_files.append(os.path.join(r,each_file))
+	return list_of_config_files
+
+
+def main():
+	list_of_tomcat=get_all_tomcats()
+	tomcat_objects=[]
+	for x in list_of_tomcat:
+		tomcat_object=Tomcat()
+		tomcat_object.get_home_conf_file(x)
+		tomcat_objects.append(tomcat_object)
+
+	#print(tomcat_objects)
+	for each_obj in tomcat_objects:
+		each_obj.display_details()
+
+
+
+if __name__=="__main__":
+	main()
